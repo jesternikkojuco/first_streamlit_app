3dbb8d6d9129c52868e5d788d7bb2e6020af703e
Jester Nikko Juco <112944248+jesternikkojuco@users.noreply.github.com>
2022-09-20 18:33:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #create the repeatable code block 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = request.get("https://fruityvice.com/api/fruit/"+fruitchoice)
+  fruityvice_response = request.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
